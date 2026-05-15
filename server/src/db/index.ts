6551ec110035b7af0d7517be6137b4484d3f0f75
jessicarams1108-blip <jessicarams1108@gmail.com>
2026-05-14 23:53:58 -0400

    Use SSL for Postgres on Render

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,10 +1,22 @@
 import pg from "pg";
 import { env } from "../env.js";
 
+/** Render-hosted Postgres (and many cloud URLs) expect TLS from Node clients. */
+function poolSslOption(): { rejectUnauthorized: boolean } | undefined {
+  const url = env.DATABASE_URL;
+  if (/\.render\.com/i.test(url) || process.env.RENDER === "true") {
+    return { rejectUnauthorized: false };
+  }
+  return undefined;
+}
+
+const ssl = poolSslOption();
+
 export const pool = new pg.Pool({
   connectionString: env.DATABASE_URL,
   max: Number(process.env.PG_POOL_MAX ?? 10),
   idleTimeoutMillis: 30_000,
+  ...(ssl !== undefined ? { ssl } : {}),
 });
 
 pool.on("error", (err) => {
